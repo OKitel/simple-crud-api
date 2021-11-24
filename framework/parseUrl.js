575c741b9d4420e21ed61edd28b18e5a5adf96db
OKitel <kitelolga@gmail.com>
2021-11-25 00:41:03 +0100

    fix: correct invalid person id  parsing path

--- framework/parseUrl.js
+++ framework/parseUrl.js
@@ -1,7 +1,6 @@
 module.exports = (baseUrl) => (request, response) => {
   const parsedUrl = new URL(request.url, baseUrl);
   const params = {};
-  // console.log(request);
   console.log(parsedUrl);
   parsedUrl.searchParams.forEach((value, key) => (params[key] = value));
   request.pathname = parsedUrl.pathname;
