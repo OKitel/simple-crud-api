f31adf176db7a6b9469033eb66a082429b30951f
OKitel <kitelolga@gmail.com>
2021-11-25 13:45:49 +0100

    feat: add delete person function

--- src/person-controller.js
+++ src/person-controller.js
@@ -97,10 +97,37 @@ const updatePartPerson = (request, response) => {
 };
 
 const deletePerson = (request, response) => {
-  //TODO
-  const person = request.body;
-  persons.push(person);
-  response.send(person);
+  const personId = request.pathParams.personId;
+  if (personId) {
+    if (
+      personId.match(
+        /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+      )
+    ) {
+      const person = persons.find((person) => person.id == personId);
+      const personIndex = persons.indexOf(person);
+      if (personIndex === -1) {
+        response.writeHead(404, {
+          "Content-type": "application/json",
+        });
+        return response.end(
+          JSON.stringify({ message: `Not found person with id = ${personId}` })
+        );
+      }
+      persons.splice(personIndex, 1);
+      response.writeHead(204, `The person was successfully deleted`, {
+        "Content-type": "application/json",
+      });
+      return response.end();
+    } else {
+      response.writeHead(400, {
+        "Content-type": "application/json",
+      });
+      response.end(
+        JSON.stringify({ message: "Bad request: incorrect id format!" })
+      );
+    }
+  }
 };
 module.exports = {
   getPersons,
