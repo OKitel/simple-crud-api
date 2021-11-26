b4aded6a282befb0ace7e5877fc0e2cb3c792337
OKitel <kitelolga@gmail.com>
2021-11-26 23:07:06 +0100

    feat: add simple tests for get and create person, add E2E test scenario #1

---
+++ __tests__/index.test.js
@@ -0,0 +1,99 @@
+const app = require("../app");
+const supertest = require("supertest");
+const request = supertest(app.application.server);
+
+it("gets the person endpoint", (done) => {
+  request
+    .get("/person")
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((response) => {
+      expect(response.body).toStrictEqual([]);
+      done();
+    })
+    .catch((err) => done(err));
+});
+
+it("create the person", (done) => {
+  request
+    .post("/person")
+    .send({ name: "John Doe", age: 33, hobbies: [] })
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(201)
+    .then((response) => {
+      const personId = response.body.id;
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          name: "John Doe",
+          age: 33,
+          hobbies: [],
+        })
+      );
+      request.delete(`/person/${personId}`).then(done());
+    })
+    .catch((err) => done(err));
+});
+
+it("E2E scenario #1", (done) => {
+  request
+    .get("/person")
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((response) => {
+      expect(response.body).toStrictEqual([]);
+    })
+    .catch((err) => done(err));
+  request
+    .post("/person")
+    .send({ name: "John Doe", age: 33, hobbies: [] })
+    .then((response) => {
+      const personId = response.body.id;
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          name: "John Doe",
+          age: 33,
+          hobbies: [],
+        })
+      );
+      request.get(`/person/${personId}`).then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            name: "John Doe",
+            age: 33,
+            hobbies: [],
+          })
+        );
+      });
+      request
+        .put(`/person/${personId}`)
+        .send({
+          name: "John Doe",
+          age: 33,
+          hobbies: ["playing piano", "horse riding"],
+        })
+        .then((response) => {
+          expect(response.body)
+            .toEqual(
+              expect.objectContaining({
+                name: "John Doe",
+                age: 33,
+                hobbies: ["playing piano", "horse riding"],
+              })
+            )
+            .then(() => {
+              request.delete(`/person/${personId}`).expect(204);
+            })
+            .then(() => {
+              request.get(`/person/${personId}`).then((response) => {
+                expect(response.body.message).toEqual("Not found!");
+              });
+            });
+        });
+
+      done();
+    })
+    .catch((err) => done(err));
+});
