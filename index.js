d882ab71d610c463f19d72734fc7ac7358ff6183
OKitel <kitelolga@gmail.com>
2021-11-26 23:06:16 +0100

    refactor: change code architecture for testing

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const Application = require("./server/application");
-const personRouter = require("./src/person-router");
-const jsonParser = require("./server/parseJson");
-const parseUrl = require("./server/parseUrl");
-const parsePathParams = require("./server/parsePathParams");
+const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-const application = new Application();
-
-application.use(jsonParser);
-application.use(parseUrl(`http://localhost:${PORT}`));
-application.use(parsePathParams(`http://localhost:${PORT}`, "/person/:id"));
-application.addRouter(personRouter);
-application.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.application.listen(app.PORT, () =>
+  console.log(`Server started on port ${app.PORT}`)
+);
